1f8bd2452d677c418764615df6d757a504701d07
LiXiaoping <xplisina@sina.com>
2018-10-16 17:25:42 +0800

    创建项目

---
+++ dynamicModel.py
@@ -0,0 +1,41 @@
+class DynamicModel:
+    nodes = []
+    pathCost = []
+
+    def initModel(self, dataLines):
+        # 首先识别全部的状态--全部的字母
+        m = dataLines.__len__() - 1
+        zimu = []
+        for i in range(0, m):
+            zimu.append(chr(ord('A') + i))
+        print(zimu)
+        # 然后识别每个状态
+        for i in range(0, m):
+            temps = dataLines[i].split(';')
+            print(temps)
+            n = temps.__len__()
+            v = []
+            vv = []
+            for j in range(0, n):
+                if temps.__len__() > 1:
+                    v.append("%c%d" % (zimu[i], j + 1))
+                else:
+                    v.append(zimu[i])
+
+                # 识别各点之间的距离
+                vtemp = temps[j].split()
+                print("距离：", vtemp)
+                nn = vtemp.__len__()
+                vvv = []
+                for k in range(0, nn):
+                    vvv.append(int(vtemp[k]))
+                vv.append(vvv)
+
+            print(v)
+            self.nodes.append(v)
+            print(vv)
+            self.pathCost.append(vv)
+
+        print(self.nodes)
+        print(self.pathCost)
+        return
---
+++ main.py
@@ -0,0 +1,15 @@
+# 动态规划程序
+
+from dynamicModel import DynamicModel
+
+filename = "p.192-8.2.dat"
+try:
+    with open(filename) as sourceFile:
+        dataLines = sourceFile.readlines()
+        print(dataLines)
+    dynamicModel = DynamicModel()
+    dynamicModel.initModel(dataLines)
+    print("计算完成...")
+except IOError:
+    print("文件不存在...")
+print("Bye...")
